524a64f24cd4f0a3d38d44311bd5a1db40ed5430
Kalpitha <kalpitharb04@gmail.com>
2023-04-06 13:00:52 +0530

    added Footer container to the page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <p>This container is for Body part</p>
       </header>
+      <Footer/> 
     </div>
   );
 }
